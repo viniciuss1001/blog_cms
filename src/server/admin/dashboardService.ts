42e2365fc3e7e1953cbfdc032a17321c6df39c82
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-04-04 22:27:51 -0300

    dashboardService

---
+++ src/server/admin/dashboardService.ts
@@ -0,0 +1,21 @@
+"use server"
+
+import { auth } from "@/lib/auth"
+import { prisma } from "@/lib/db"
+
+export const getDashboardData = async ({ blogId }: { blogId: string }) => {
+	const session = await auth()
+
+	const totalUsers = await prisma.blogUser.count({ where: { blogId } })
+	const totalPosts = await prisma.post.count({ where: { blogId } })
+	const totalPostsMadeByYou = await prisma.post.count({
+		where: {
+			blogId,
+			userId: session?.user?.id
+		}
+	})
+
+	return {
+		totalUsers, totalPosts, totalPostsMadeByYou
+	}
+}
